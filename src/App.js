2da8194903ccf30e30256b8f96c96922b345d013
mrynp <110476086+mrynp@users.noreply.github.com>
2024-09-11 13:21:16 +0800

    Add input functionality to add exercises and rest time to workout plan

--- src/App.js
+++ src/App.js
@@ -2,25 +2,14 @@ import React from "react";
 // import SearchExercise from "./SearchExercise";
 import WorkoutInput from "./WorkoutInput";
 import WorkoutList from "./WorkoutList";
-
 import "./App.css";
 import { useState } from "react";
 
 function App() {
-  // const [workoutPlan, setWorkoutPlan] = useState([]);
-
-  // const addExerciseToWorkout = (exercise) => {
-  //   setWorkoutPlan((prevPlan) => [...prevPlan, exercise]);
-  // };
-  const workoutPlan = [
+  const [workoutPlan, setWorkoutPlan] = useState([
     {
       type: "exercise",
       name: "pull up",
-      time: 30,
-    },
-    {
-      type: "exercise",
-      name: "push up",
       time: 30,
     },
     {
@@ -28,12 +17,26 @@ function App() {
       name: "rest",
       time: 30,
     },
-  ];
+    {
+      type: "exercise",
+      name: "push up",
+      time: 30,
+    },
+  ]);
+
+  // function handleAddExercise() {
+  //   const newWorkout = [...workoutPlan, newWorkout];
+  //   setWorkoutPlan(newWorkout);
+  // }
+
+  const addExerciseToWorkout = (exercise) => {
+    setWorkoutPlan((prevPlan) => [...prevPlan, exercise]);
+  };
 
   return (
     <div className="App">
       <h1>Workout Timer</h1>
-      <WorkoutInput />
+      <WorkoutInput onAddExercise={addExerciseToWorkout} />
       <WorkoutList workoutPlan={workoutPlan} />
       {/* <SearchExercise onAddExercise={addExerciseToWorkout} /> */}
     </div>
--- src/WorkoutInput.js
+++ src/WorkoutInput.js
@@ -1,21 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
-const WorkoutInput = (props) => {
+const WorkoutInput = ({ onAddExercise }) => {
+  const [exerciseName, setExerciseName] = useState("");
+  const [exerciseTime, setExerciseTime] = useState("");
+  const [restTime, setRestTime] = useState("");
+
+  const handleAddExercise = () => {
+    if (exerciseName && exerciseTime) {
+      onAddExercise({
+        type: "exercise",
+        name: exerciseName,
+        time: parseInt(exerciseTime), // Default rest time to 0 if not specified
+      });
+      setExerciseName("");
+      setExerciseTime("");
+    }
+  };
+
+  const handleAddRest = () => {
+    onAddExercise({
+      type: "rest",
+      name: "rest",
+      time: parseInt(restTime), // Default rest time to 0 if not specified
+    });
+    setRestTime("");
+  };
+
   return (
     <div>
       <div className="input-container">
         <div>
           <input
+            value={exerciseName}
             className="exercise-input"
+            onChange={(e) => setExerciseName(e.target.value)}
             type="text"
             placeholder="Add an exercise..."
           />
-          <input className="time-input" type="number" placeholder="Time (s)" />
-          <button>Add Exercise</button>
+          <input
+            value={exerciseTime}
+            className="time-input"
+            onChange={(e) => setExerciseTime(e.target.value)}
+            type="number"
+            placeholder="Time (s)"
+          />
+          <button onClick={handleAddExercise}>Add Exercise</button>
         </div>
         <div>
-          <input className="time-input" type="number" placeholder="Time (s)" />
-          <button>Add Rest</button>
+          <input
+            value={restTime}
+            className="time-input"
+            onChange={(e) => setRestTime(e.target.value)}
+            type="number"
+            placeholder="Time (s)"
+          />
+          <button onClick={handleAddRest}>Add Rest</button>
         </div>
       </div>
     </div>
