3688f59e001db909e73cef77490bdd0a5c89f96f
mrynp <110476086+mrynp@users.noreply.github.com>
2024-09-19 12:25:20 +0800

    Add timer functionality

--- src/WorkoutTimer.js
+++ src/WorkoutTimer.js
@@ -1,33 +1,48 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const WorkoutTimer = ({ workoutPlan }) => {
   const [currentExerciseIndex, setCurrentExerciseIndex] = useState(0);
   const [timeRemaining, setTimeRemaining] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
 
+  useEffect(() => {
+    let timer;
+    if (isRunning && timeRemaining > 0) {
+      timer = setInterval(() => {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      }, 1000);
+    } else if (timeRemaining === 0 && isRunning) {
+      if (currentExerciseIndex < workoutPlan.length - 1) {
+        setCurrentExerciseIndex((prevIndex) => prevIndex + 1);
+        setTimeRemaining(workoutPlan[currentExerciseIndex + 1].time);
+      } else {
+        setIsRunning(false);
+      }
+    }
+    return () => clearInterval(timer);
+  }, [isRunning, timeRemaining, currentExerciseIndex, workoutPlan]);
+
   const startWorkout = () => {
     if (workoutPlan.length > 0) {
       setCurrentExerciseIndex(0);
       setTimeRemaining(workoutPlan[0].time);
       setIsRunning(true);
-
-      console.log(currentExercise.name);
-      console.log(nextExercise.name);
-      console.log(timeRemaining);
-      console.log(isRunning);
     }
   };
 
   const currentExercise = workoutPlan[currentExerciseIndex];
-  const nextExercise = workoutPlan[currentExerciseIndex + 1];
+  const nextExercise =
+    currentExerciseIndex < workoutPlan.length - 1
+      ? workoutPlan[currentExerciseIndex + 1]
+      : null;
 
   return (
     <div className="timer-container">
       {isRunning && (
         <div className="timer-display">
           <p>{timeRemaining}</p>
-          <p>{currentExercise.name}</p>
-          <p>{nextExercise.name}</p>
+          <p>{currentExercise?.name}</p>
+          {nextExercise && <p> {nextExercise.name}</p>}
         </div>
       )}
 
