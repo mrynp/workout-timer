7bae8d8084fb4288dddb9f75675afa94bf68319e
mrynp <110476086+mrynp@users.noreply.github.com>
2024-10-09 16:36:22 +0800

    Remove rest time input from workout form

--- src/WorkoutInput.js
+++ src/WorkoutInput.js
@@ -8,8 +8,6 @@ const WorkoutInput = ({
   exerciseTime,
   setExerciseTime,
 }) => {
-  const [restTime, setRestTime] = useState("");
-
   const capitalizeFirstLetter = (string) => {
     return string.toLowerCase().replace(/\b\w/g, (char) => char.toUpperCase());
   };
@@ -22,24 +20,6 @@ const WorkoutInput = ({
       });
       setExerciseName("");
       setExerciseTime("");
-    }
-  };
-
-  const handleAddRest = () => {
-    if (
-      workoutPlan.length > 0 &&
-      workoutPlan[workoutPlan.length - 1].name === "rest"
-    ) {
-      alert("Cannot add consecutive rest periods.");
-      return;
-    }
-
-    if (restTime) {
-      onAddExercise({
-        name: "Rest",
-        time: parseInt(restTime),
-      });
-      setRestTime("");
     }
   };
 
@@ -65,33 +45,6 @@ const WorkoutInput = ({
           <button
             onClick={() => {
               handleAddExercise();
-            }}
-          >
-            Add
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              width="15"
-              height="15"
-              fill="rgba(228,228,231,1)"
-            >
-              <path d="M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z"></path>
-            </svg>
-          </button>
-        </div>
-        <div>
-          <div className="rest-input">Rest</div>
-          <input
-            value={restTime}
-            className="time-input"
-            onChange={(e) => setRestTime(e.target.value)}
-            type="number"
-            placeholder="Time (s)"
-            required
-          />
-          <button
-            onClick={() => {
-              handleAddRest();
             }}
           >
             Add
