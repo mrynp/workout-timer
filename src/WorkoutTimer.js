6ecba365b7b1393972651be1ce296a8acdcb2b6a
mrynp <110476086+mrynp@users.noreply.github.com>
2024-10-09 12:51:42 +0800

    Update icon fill color for start and pause buttons based on active state

--- src/WorkoutTimer.js
+++ src/WorkoutTimer.js
@@ -54,12 +54,15 @@ const WorkoutTimer = ({ workoutPlan }) => {
             <div className="btn-padding">
               <button className="custom-btn" onClick={startWorkout}>
                 <svg
-                  className="icon"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
                   width="16"
                   height="16"
-                  fill=" rgba(255, 255, 255, 0.4)"
+                  fill={`${
+                    isRunning
+                      ? "rgba(254, 243, 200, 0.9)"
+                      : "rgba(255, 255, 255, 0.4)"
+                  }`}
                 >
                   <path d="M9 8.48216V15.518L15.0307 12.0001L9 8.48216ZM7.75194 5.43872L18.2596 11.5682C18.4981 11.7073 18.5787 12.0135 18.4396 12.252C18.3961 12.3265 18.3341 12.3885 18.2596 12.432L7.75194 18.5615C7.51341 18.7006 7.20725 18.62 7.06811 18.3815C7.0235 18.305 7 18.2181 7 18.1296V5.87061C7 5.59446 7.22386 5.37061 7.5 5.37061C7.58853 5.37061 7.67547 5.39411 7.75194 5.43872Z"></path>
                 </svg>
@@ -69,14 +72,20 @@ const WorkoutTimer = ({ workoutPlan }) => {
           </div>
           <div className="btn-container">
             <div className="btn-padding">
-              <button className="custom-btn" onClick={pauseWorkout}>
+              <button
+                className={`custom-btn ${!isRunning ? "icon" : ""}`}
+                onClick={pauseWorkout}
+              >
                 <svg
-                  className="icon"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
                   width="16"
                   height="16"
-                  fill=" rgba(255, 255, 255, 0.4)"
+                  fill={`${
+                    !isRunning
+                      ? "rgba(254, 243, 200, 0.9)"
+                      : "rgba(255, 255, 255, 0.4)"
+                  }`}
                 >
                   <path d="M15 7C15 6.44772 15.4477 6 16 6C16.5523 6 17 6.44772 17 7V17C17 17.5523 16.5523 18 16 18C15.4477 18 15 17.5523 15 17V7ZM7 7C7 6.44772 7.44772 6 8 6C8.55228 6 9 6.44772 9 7V17C9 17.5523 8.55228 18 8 18C7.44772 18 7 17.5523 7 17V7Z"></path>
                 </svg>
@@ -88,7 +97,6 @@ const WorkoutTimer = ({ workoutPlan }) => {
             <div className="btn-padding">
               <button className="custom-btn">
                 <svg
-                  className="icon"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
                   width="12"
